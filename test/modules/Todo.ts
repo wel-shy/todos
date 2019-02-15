29051c8434083892531eec289adfff15bd4abd3d
Daniel Welsh <d.welsh@newcastle.ac.uk>
2019-02-15 13:46:46 +0100

    :sparkles: Implementing remaining methods on mongo resource controller

--- test/modules/Todo.ts
+++ test/modules/Todo.ts
@@ -1,16 +1,25 @@
 import {expect} from "chai";
+import {Schema} from "mongoose";
 import ControllerFactory from "../../web/controllers/ControllerFactory";
 import {IResourceController} from "../../web/controllers/IResourceController";
+import CryptoHelper from "../../web/CryptoHelper";
 import {ITodo} from "../../web/schemas/Todo";
 import {IUser} from "../../web/schemas/User";
-import CryptoHelper from "../../web/CryptoHelper";
 
 describe("todo", () => {
   describe("Controller", () => {
     const userController: IResourceController<IUser> = ControllerFactory.getController("user");
     const todoController: IResourceController<ITodo> = ControllerFactory.getController("todo");
     let user: IUser;
+    let secondUser: IUser;
     let todo: ITodo;
+    let secondTodo: ITodo;
+    let updateTodo: ITodo;
+    let updateTodoData: any = {
+      done: false,
+      task: "test todo",
+      userId: new Schema.Types.ObjectId(""),
+    };
 
     before(async () => {
       user = await userController.store({
@@ -18,10 +27,25 @@ describe("todo", () => {
         password: "secret",
         username: "tester-todo",
       });
+
+      secondUser = await userController.store({
+        iv: CryptoHelper.getRandomString(16),
+        password: "secret",
+        username: "tester-todo-2",
+      });
+
+      updateTodoData.userId = user._id;
+      updateTodo = await todoController.store(updateTodoData);
+
+      updateTodoData.userId = secondUser._id;
+      secondTodo = await todoController.store(updateTodoData);
     });
 
     after(async () => {
-      userController.destroy(user._id);
+      await userController.destroy(user._id);
+      await userController.destroy(secondUser._id);
+      await todoController.destroy(updateTodo._id);
+      await todoController.destroy(secondTodo._id);
     });
 
     it("Should create a todo item", (done) => {
@@ -36,8 +60,34 @@ describe("todo", () => {
           todo = newTodo;
           done();
         })
-        .catch((e) => {
-          console.error(e);
+    });
+
+    it("Should get a todo item", (done) => {
+      todoController.get(todo._id)
+        .then((t: ITodo) => {
+          expect(t.task).to.equal(todo.task);
+          done();
+        })
+    });
+
+    it("Should get all todo items", (done) => {
+      todoController.getAll()
+        .then((todos: ITodo[]) => {
+          expect(todos.length).to.be.greaterThan(0);
+          done();
+        })
+    });
+
+    it("Should update todo item", (done) => {
+      updateTodoData = {
+        done: true,
+      };
+
+      todoController.edit(updateTodo._id, updateTodoData)
+        .then((newTodo: ITodo) => {
+          expect(newTodo.done).to.equal(true);
+          expect(newTodo.task).to.equal("test todo");
+          done();
         })
     });
 
@@ -50,9 +100,16 @@ describe("todo", () => {
               done();
             });
         })
-        .catch((e) => {
-          console.error(e);
-        })
     });
+
+    it("Should fetch only the users todos", (done) => {
+      todoController.findManyWithFilter({userId: secondUser._id})
+        .then((todos: ITodo[]) => {
+          todos.forEach((t) => {
+            expect(t.userId.toString()).to.equal(secondUser._id.toString());
+          });
+          done();
+        })
+    })
   });
 });
--- web/controllers/MongoResourceController.ts
+++ web/controllers/MongoResourceController.ts
@@ -6,38 +6,81 @@ import {IResourceController} from "./IResourceController";
 export class MongoController<T extends IBaseMongoResource> implements IResourceController<T> {
   private table: string;
 
+  /**
+   * Destroy a record
+   * @param {Schema.Types.ObjectId} id
+   * @returns {Promise<void>}
+   */
   public async destroy(id: Schema.Types.ObjectId): Promise<void> {
     await getModel(this.getTableName()).deleteOne({_id: id});
   }
 
-  public edit(id: Schema.Types.ObjectId, data: any): Promise<T> {
-    return undefined;
+  /**
+   * Update a record
+   * @param {Schema.Types.ObjectId} id
+   * @param {{}} data
+   * @returns {Promise<T>}
+   */
+  public async edit(id: Schema.Types.ObjectId, data: {}): Promise<T> {
+    return await getModel(this.getTableName()).findByIdAndUpdate(id, data, {new: true}) as T;
   }
 
+  /**
+   * Get a record
+   * @param {Schema.Types.ObjectId} id
+   * @returns {Promise<T>}
+   */
   public async get(id: Schema.Types.ObjectId): Promise<T> {
     return await getModel(this.getTableName()).findOne({_id: id}) as T;
   }
 
+  /**
+   * Find a record matching search params
+   * @param {{}} filter
+   * @returns {Promise<T[]>}
+   */
   public async findManyWithFilter(filter: {}): Promise<T[]> {
     return await getModel(this.getTableName()).find(filter) as T[];
   }
 
+  /**
+   * Find many matching records with search params
+   * @param {{}} filter
+   * @returns {Promise<T>}
+   */
   public async findOneWithFilter(filter: {}): Promise<T> {
     return await getModel(this.getTableName()).findOne(filter) as T;
   }
 
-  public getAll(): Promise<T[]> {
-    return undefined;
+  /**
+   * Get all records
+   * @returns {Promise<T[]>}
+   */
+  public async getAll(): Promise<T[]> {
+    return await getModel(this.getTableName()).find() as T[];
   }
 
+  /**
+   * Store a record.
+   * @param data
+   * @returns {Promise<T>}
+   */
   public async store(data: any): Promise<T> {
     return await getModel(this.getTableName()).create(data) as T;
   }
 
+  /**
+   * Get the table name for record type.
+   * @returns {string}
+   */
   public getTableName(): string {
     return this.table;
   }
 
+  /**
+   * Set the table name for record type.
+   * @param {string} table
+   */
   public setTableName(table: string): void {
     this.table = table;
   }
